76b3b375976830f09d278bb0b15541c1cd8dfe5d
unknown <natalie9705@gmail.com>
2024-10-02 19:32:55 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -146,14 +146,14 @@ private:
 		uint16_t triangles[] = {
 			0, 1, 2,
 			0, 2, 11,
-			//2, 3, 4,
-			//2, 4, 5,
-			//8, 5, 6,
-			//8, 6, 7,
-			//10, 11, 8,
-			//20, 8, 9, 
-			//11, 2, 5,
-			//11, 5, 8
+			2, 3, 4,
+			2, 4, 5,
+			8, 5, 6,
+			8, 6, 7,
+			10, 11, 8,
+			20, 8, 9, 
+			11, 2, 5,
+			11, 5, 8
 		};
 		// TODO: Part 4a
 		CreateIndexBuffer(&triangles[0], sizeof(triangles));
@@ -544,7 +544,7 @@ public:
 		SetUpPipelineTriangle(commandBuffer, rotation);
 		
 		// TODO: Part 3d
-		vkCmdDraw(commandBuffer, 6, 1, 0, 0); // TODO: Part 1b, Part 1c
+		vkCmdDrawIndexed(commandBuffer, 30, 1, 0, 0, 0); // TODO: Part 1b, Part 1c
 		// TODO: Part 4g
 	}
 
