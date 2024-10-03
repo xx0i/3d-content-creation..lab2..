568d556e5fba3ebc09d15b38e10b2dd7606f9564
unknown <natalie9705@gmail.com>
2024-10-03 17:54:43 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -619,11 +619,16 @@ public:
 		// TODO: Part 2b
 		shaderVars rotation{};
 		rotation.worldMatrix = zRotationMatrix;
-		VkCommandBuffer commandBuffer = GetCurrentCommandBuffer();
-		SetUpPipelineTriangle(commandBuffer, rotation);
 
+		VkCommandBuffer commandBuffer = GetCurrentCommandBuffer();
+
+		SetUpPipeline(commandBuffer, rotation);
+		vkCmdDraw(commandBuffer, 13, 1, 0, 0); // TODO: Part 1b, Part 1c
+
+		SetUpPipelineTriangle(commandBuffer, rotation);
 		// TODO: Part 3d
 		vkCmdDrawIndexed(commandBuffer, 30, 1, 0, 0, 0);
+
 		SetUpPipeline(commandBuffer, rotation);
 		vkCmdDraw(commandBuffer, 13, 1, 0, 0); // TODO: Part 1b, Part 1c
 	}
