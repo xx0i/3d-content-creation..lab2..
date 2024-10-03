d2cca05256e3596b5377466faab3318a2c63936e
unknown <natalie9705@gmail.com>
2024-10-03 11:25:11 -0400

    auto commit

--- renderer.h
+++ renderer.h
@@ -329,7 +329,7 @@ private:
 		// Create Stage Info for Fragment Shader
 		stage_create_info[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
 		stage_create_info[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-		stage_create_info[1].module = fragmentShader; // TODO: Part 4f, Part 4g
+		stage_create_info[1].module = fragmentShader2; // TODO: Part 4f, Part 4g
 		stage_create_info[1].pName = "main";
 
 
@@ -623,7 +623,7 @@ public:
 		SetUpPipelineTriangle(commandBuffer, rotation);
 		
 		// TODO: Part 3d
-		vkCmdDrawIndexed(commandBuffer, 30, 1, 0, 0, 0);
+		vkCmdDrawIndexed(commandBuffer, 32, 1, 0, 0, 0);
 		// TODO: Part 4g
 	}
 
